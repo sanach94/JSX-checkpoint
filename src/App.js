70419d4c90e4bda0451b629779cfec82e4573c70
sanachouchan <chouchane.sana.gec@gmail.com>
2020-07-14 10:09:19 +0100

    JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import myWonderfulImage from "./gomycode.png";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ border: "solid 1 black", maxWidth: "100vw" }} />
+      <h1 className="title red"> Your name here </h1>
+      <br />
+      <img src="/Gomycode-black.jpg" /> <br />
+      <img src={myWonderfulImage} />
+      <br />
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/UcycZretpbc"
+        frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
     </div>
   );
 }
